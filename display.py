98aca29e83b0ec9a573e4727faa5c53f5f82421a
julietbravo <bart@vanstratum.com>
2019-11-07 11:05:09 +0000

    Cleanup display code, wrapped everything inside a class

--- display.py
+++ display.py
@@ -1,100 +1,75 @@
 import smbus
 import time
 import datetime
-import subprocess
 
-I2C_ADDR  = 0x3F # I2C device address
-LCD_WIDTH = 20   # Maximum characters per line
+class LCD:
+    def __init__(self, i2c_addr, sm_bus):
+        self.i2c_addr = i2c_addr
+        self.sm_bus = sm_bus
 
-# Define some device constants
-LCD_CHR = 1 # Mode - Sending data
-LCD_CMD = 0 # Mode - Sending command
+        self.lcd_width = 20     # Width (characters) display
 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
-LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+        self.lcd_chr = 1        # Mode - Sending data
+        self.lcd_cmd = 0        # Mode - Sending command
+
+        self.e_pulse = 0.0005
+        self.e_delay = 0.0005
+
+        self.bus = smbus.SMBus(self.sm_bus)
+
+        # Init display
+        self.lcd_byte(0x33, self.lcd_cmd)  # 110011 Initialise
+        self.lcd_byte(0x32, self.lcd_cmd)  # 110010 Initialise
+        self.lcd_byte(0x06, self.lcd_cmd)  # 000110 Cursor move direction
+        self.lcd_byte(0x0C, self.lcd_cmd)  # 001100 Display On, Cursor Off, Blink Off
+        self.lcd_byte(0x28, self.lcd_cmd)  # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01, self.lcd_cmd)  # 000001 Clear display
+        time.sleep(self.e_delay)
 
 
-LCD_BACKLIGHT  = 0x08  # On 0X08 / Off 0x00
+    def lcd_byte(self, bits, mode):
+        lcd_backlight  = 0x08  # on 0x08 / off 0x00
 
-ENABLE = 0b00000100 # Enable bit
+        bits_high = mode | (bits & 0xF0)      | lcd_backlight
+        bits_low  = mode | ((bits<<4) & 0xF0) | lcd_backlight
 
-E_PULSE = 0.0005
-E_DELAY = 0.0005
+        self.bus.write_byte(self.i2c_addr, bits_high)
+        self.toggle_enable(bits_high)
 
-bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+        self.bus.write_byte(self.i2c_addr, bits_low)
+        self.toggle_enable(bits_low)
 
-def run_cmd(cmd):
-    return subprocess.check_output(cmd, shell=True).decode('utf-8')
 
-def get_my_ipwlan():
-    val = run_cmd("/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'")[:-1]
-    if val == "":
-        val = "No connection!"
-    return val
+    def toggle_enable(self, bits):
+        enable = 0b00000100
 
-def get_my_ipeth():
-    val = run_cmd("/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'")[:-1]
-    if val == "":
-        val = "No connection"
-    return val
+        time.sleep(self.e_delay)
+        self.bus.write_byte(self.i2c_addr, (bits | enable))
+        time.sleep(self.e_pulse)
+        self.bus.write_byte(self.i2c_addr, (bits & ~enable))
+        time.sleep(self.e_delay)
 
-def lcd_init():
-    lcd_byte(0x33, LCD_CMD) # 110011 Initialise
-    lcd_byte(0x32, LCD_CMD) # 110010 Initialise
-    lcd_byte(0x06, LCD_CMD) # 000110 Cursor move direction
-    lcd_byte(0x0C, LCD_CMD) # 001100 Display On, Cursor Off, Blink Off
-    lcd_byte(0x28, LCD_CMD) # 101000 Data length, number of lines, font size
-    lcd_byte(0x01, LCD_CMD) # 000001 Clear display
-    time.sleep(E_DELAY)
 
-def lcd_byte(bits, mode):
+    def write(self, message, line):
+        message = message.ljust(self.lcd_width, ' ')
 
-    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-    bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+        if line == 0:
+            self.lcd_byte(0x80, self.lcd_cmd)
+        elif line == 1:
+            self.lcd_byte(0xC0, self.lcd_cmd)
+        elif line == 2:
+            self.lcd_byte(0x94, self.lcd_cmd)
+        elif line == 3:
+            self.lcd_byte(0xD4, self.lcd_cmd)
 
-    bus.write_byte(I2C_ADDR, bits_high)
-    lcd_toggle_enable(bits_high)
+        for i in range(self.lcd_width):
+            self.lcd_byte(ord(message[i]), self.lcd_chr)
 
-    bus.write_byte(I2C_ADDR, bits_low)
-    lcd_toggle_enable(bits_low)
-
-def lcd_toggle_enable(bits):
-    time.sleep(E_DELAY)
-    bus.write_byte(I2C_ADDR, (bits | ENABLE))
-    time.sleep(E_PULSE)
-    bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-    time.sleep(E_DELAY)
-
-def lcd_string(message,line):
-
-    message = message.ljust(LCD_WIDTH," ")
-
-    lcd_byte(line, LCD_CMD)
-
-    for i in range(LCD_WIDTH):
-      lcd_byte(ord(message[i]),LCD_CHR)
-
-def main():
-
-    lcd_init()
-
-    while True:
-
-        now = datetime.datetime.now()
-
-        lcd_string("test", LCD_LINE_1)
-        lcd_string( str(now.day)+'/'+str(now.month)+'/'+str(now.year)+' '+str(now.hour)+':'+str(now.minute),LCD_LINE_2)
-        lcd_string("W:{}".format(get_my_ipwlan()),LCD_LINE_3)
-        lcd_string("E:{}".format(get_my_ipeth()),LCD_LINE_4)
-        time.sleep(60)
 
 if __name__ == '__main__':
 
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        lcd_byte(0x01, LCD_CMD)
+    lcd = LCD(i2c_addr=0x27, sm_bus=1)
+    lcd.write('hello world!', 0)
+    lcd.write('hello world!', 1)
+    lcd.write('hello world!', 2)
+    lcd.write('hello world!', 3)
